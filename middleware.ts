5093cfa9ba76dd96e6a00b56e02acff22cf489a2
rafidulislam19 <you@example.com>
2025-02-04 15:42:36 +0600

    update

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,36 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+// import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware({
-  publicRoutes: ["/api/webhook"]
+// export default clerkMiddleware({
+//   publicRoutes: ["/api/webhook"]
+// });
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     // Always run for API routes
+//     '/(api|trpc)(.*)',
+//   ],
+// }
+
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware((auth, req) => {
+  const publicPaths = ["/api/webhook"];
+
+  // Check if the request is for a public route
+  if (publicPaths.includes(req.nextUrl.pathname)) {
+    return;
+  }
+
+  auth();
 });
 
+// Config for matching routes
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
   ],
-}
+};
+
